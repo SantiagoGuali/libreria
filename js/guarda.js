8b07e6166d7e3f7133bf660251839ee5340281bc
SantiagoGuali <137077371+SantiagoGuali@users.noreply.github.com>
2024-05-20 16:58:33 -0500

    s

--- js/guarda.js
+++ js/guarda.js
@@ -67,11 +67,74 @@ function verTabla() {
                 <td>${libro.Disponibilidad_libro}</td>
                 <td>${libro.paginasN_libro}</td>
                 <td>
-                <button class="btn btn-outline-danger btn-sm" onclick="deleteLibro('${doc.id}')">Eliminar</button>
-                <button class="btn btn-outline-success btn-sm" onclick="">Actualizar</button>
+                    <button class="btn btn-outline-danger btn-sm" onclick="deleteLibro('${doc.id}')">Eliminar</button>
+                   
                 </td>
+                <td><button class="btn btn-outline-success btn-sm" onclick="editLibro('${doc.id}')">Actualizar</button></td>
+                
             `;
             librosTbody.appendChild(libroRow);
         });
-    })
+    });
 }
+
+
+
+function editLibro(id) {
+    const libroDoc = db.collection("Libros").doc(id);
+    libroDoc.get().then((doc) => {
+        if (doc.exists) {
+            const libro = doc.data();
+            document.getElementById('libro-id').value = id;
+            document.getElementById('nombre-libro').value = libro.nombre_libro;
+            document.getElementById('autor-libro').value = libro.autor_libro;
+            document.getElementById('idioma-libro').value = libro.Idioma;
+            document.getElementById('descripcion-libro').value = libro.descripcion_libro;
+            document.getElementById('editorial-libro').value = libro.editorial_libro;
+            document.getElementById('fechaP-libro').value = libro.fechaP_libro;
+            document.getElementById('genero-libro').value = libro.genero_libro;
+            document.getElementById('disponibilidad-libro').value = libro.Disponibilidad_libro;
+            document.getElementById('paginasN-libro').value = libro.paginasN_libro;
+            $('#updateModal').modal('show');
+             
+        }
+    });
+}
+
+
+function updateLibro() {
+    const id = document.getElementById('libro-id').value;
+    const libroActualizado = {
+        nombre_libro: document.getElementById('nombre-libro').value,
+        autor_libro: document.getElementById('autor-libro').value,
+        Idioma: document.getElementById('idioma-libro').value,
+        descripcion_libro: document.getElementById('descripcion-libro').value,
+        editorial_libro: document.getElementById('editorial-libro').value,
+        fechaP_libro: document.getElementById('fechaP-libro').value,
+        genero_libro: document.getElementById('genero-libro').value,
+        Disponibilidad_libro: document.getElementById('disponibilidad-libro').value,
+        paginasN_libro: document.getElementById('paginasN-libro').value
+    };
+    db.collection("Libros").doc(id).update(libroActualizado).then(() => {
+        $('#updateModal').modal('hide');
+        alert("Datos actualizados correctamente");
+        verTabla();
+    }).catch((error) => {
+        console.error("Error al actualizar el libro: ", error);
+    });
+}
+
+function deleteLibro(id) {
+    db.collection("Libros").doc(id).delete().then(() => {
+        verTabla();
+    }).catch((error) => {
+        console.error("Error al eliminar el libro: ", error);
+    });
+}
+
+function btclose()
+{
+    $('#updateModal').modal('hide');
+}
+
+
